73791294deea21cb36b1f5751bdc885d3de3ee4f
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-12-17 18:23:32 +0530

    added comments

--- ArchBuild_1.sh
+++ ArchBuild_1.sh
@@ -3,8 +3,16 @@
 pacman -Sy git --noconfirm
 timedatectl set-ntp true
 
-# Partition 1: /dev/sda1 -> boot partition. Size: 512 MB.
-# Partition 2: /dev/sda2 -> root partition. Size: rest of the disk.
+# Partition 1: /dev/sda1 -> boot partition.
+# Partition 2: /dev/sda2 -> root partition.
+# /dev/sda1 :
+#   Size: 512 MB. Sector: 2048 to 1050632
+#   Type: UEFI
+#   Bootable: Yes
+# /dev/sda2
+#   Size : Rest of the disk. Sector: Rest of the disk.
+#   Type : Linux
+#   Bootable: No
 fdisk /dev/sda
 
 mkfs.vfat -F32 /dev/sda1
