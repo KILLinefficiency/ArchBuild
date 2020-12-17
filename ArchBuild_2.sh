8d06e59fd67416b52f56be12721ad682849ea095
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-12-18 00:22:17 +0530

    added install support for aur (yay)

--- ArchBuild_2.sh
+++ ArchBuild_2.sh
@@ -16,11 +16,20 @@ echo "127.0.0.1 localhost corvo" >> /etc/hosts
 
 passwd root
 
-useradd -m -d /home/shreyas Shreyas --force-badname
+useradd -m -d /home/shreyas Shreyas --badnames
 passwd Shreyas
 echo "Shreyas ALL=(ALL) ALL" >> /etc/sudoers
 
 grub-install /dev/sda --efi-directory="/boot"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo pacman -S xorg xorg-init --noconfirm
+sudo pacman -S xorg xorg-xinit --noconfirm
+
+cd ~
+mkdir Documents Downloads Music Videos Pictures .aur
+cd .aur
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+exit
