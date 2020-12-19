50fa2ae3ebb54627480004a3431f4e69bf721b1b
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-12-19 16:41:28 +0530

    fixed username

--- ArchBuild_2.sh
+++ ArchBuild_2.sh
@@ -17,8 +17,8 @@ echo "127.0.0.1 localhost corvo" >> /etc/hosts
 passwd root
 
 useradd -m -d /home/shreyas shreyas
-passwd Shreyas
-echo "Shreyas ALL=(ALL) ALL" >> /etc/sudoers
+passwd shreyas
+echo "shreyas ALL=(ALL) ALL" >> /etc/sudoers
 
 grub-install /dev/sda --efi-directory="/boot"
 grub-mkconfig -o /boot/grub/grub.cfg
