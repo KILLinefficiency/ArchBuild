1eaa44338417b8fb7900eac5d58b6a909a4c8f74
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-12-31 04:24:22 +0530

    minor changes

--- ArchBuild_3.sh
+++ ArchBuild_3.sh
@@ -10,28 +10,12 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-if ![ -f ~/.config ]; then
-  mkdir ~/.config
-fi
-
-mkdir ~/.config/alacritty
+mkdir -p ~/.config/alacritty
 
 cd $ADDRESS
 
-cp dotfiles/.bashrc ~/.bashrc
-cp dotfiles/.xinitrc ~
+cp dotfiles/bashrc ~/.bashrc
+cp dotfiles/xinitrc ~/.xinitrc
+cp dotfiles/vimrc ~/.vimrc
 cp dotfiles/alacritty/alacritty.yml ~/config/alacritty/alacritty.yml
 
-cd ~
-mkdir ~/.suckless
-cd ~/.suckless
-git clone git://git.suckless.org/dwm
-git clone git://git.suckless.org/dmenu
-cd ~/.suckless/dmenu
-make
-cd ~
-cd ~/.suckless/dwm
-cp $ADDRESS/dotfiles/dwm/* ~/.suckless/dwm/
-make
-
-sudo pacman -S gcc python3 python-pip --noconfirm
