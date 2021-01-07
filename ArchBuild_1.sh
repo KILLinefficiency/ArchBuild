bfffa482bac9de4bef899adf8aedddbf812b53c7
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-07 21:42:04 +0530

    added variables for disk, boot and root partition

--- ArchBuild_1.sh
+++ ArchBuild_1.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
+DISK="sda"
+BOOT="sda1"
+ROOT="sda2"
+
 pacman -Sy git --noconfirm
 timedatectl set-ntp true
 
 # Partition 1: /dev/sda1 -> boot partition.
 # Partition 2: /dev/sda2 -> root partition.
-# /dev/sda1 :
+# /dev/sdX1 :
 #   Size: 512 MB. Sector: 2048 to 1050632
 #   Type: UEFI
 #   Bootable: Yes
-# /dev/sda2
+# /dev/sdX2
 #   Size : Rest of the disk. Sector: Rest of the disk.
 #   Type : Linux
 #   Bootable: No
-fdisk /dev/sda
+fdisk /dev/${DISK}
 
-mkfs.vfat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
+mkfs.vfat -F32 /dev/${BOOT}
+mkfs.ext4 /dev/${ROOT}
 
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-mount /dev/sda2 /mnt
+mount /dev/${BOOT} /mnt/boot
+mount /dev/${ROOT} /mnt
 
 pacstrap /mnt base linux linux-firmware
 
