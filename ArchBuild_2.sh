30061585ac366138317b55c56d56e765abdf9d87
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-07 21:42:39 +0530

    used parametric substitution

--- ArchBuild_2.sh
+++ ArchBuild_2.sh
@@ -9,23 +9,23 @@ pacman -Sy git dhcpcd networkmanager grub efibootmgr base-devel --noconfirm
 systemctl enable dhcpcd
 systemctl enable NetworkManager
 
-ln -sf /usr/share/zoneinfo/$MYTIMEZONE /etc/localtime
+ln -sf /usr/share/zoneinfo/${MYTIMEZONE} /etc/localtime
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
-echo "$MYHOSTNAME" >> /etc/hostname
-echo "127.0.0.1 localhost $MYHOSTNAME" >> /etc/hosts
+echo "${MYHOSTNAME}" >> /etc/hostname
+echo "127.0.0.1 localhost ${MYHOSTNAME}" >> /etc/hosts
 
 clear
 echo "Set Root Password..."
 passwd root
 
-useradd -m -d /home/$MYUSERNAME $MYUSERNAME --badnames
-echo "Set Password for $MYUSERNAME..."
+useradd -m -d /home/${MYUSERNAME} ${MYUSERNAME} --badnames
+echo "Set Password for ${MYUSERNAME}..."
 passwd shreyas
-echo "$MYUSERNAME ALL=(ALL) ALL" >> /etc/sudoers
+echo "${MYUSERNAME} ALL=(ALL) ALL" >> /etc/sudoers
 
 grub-install /dev/sda --efi-directory="/boot"
 grub-mkconfig -o /boot/grub/grub.cfg
