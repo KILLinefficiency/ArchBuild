846d35b556eb97e2a9c708519ccfcdc9454688f8
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-07 09:39:49 +0530

    added variables for username hostname and timezone

--- ArchBuild_2.sh
+++ ArchBuild_2.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+MYUSERNAME="shreyas"
+MYHOSTNAME="batcave"
+MYTIMEZONE="Asia/Kolkata"
+
 pacman -Sy git dhcpcd networkmanager grub efibootmgr base-devel --noconfirm
 
 systemctl enable dhcpcd
 systemctl enable NetworkManager
 
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+ln -sf /usr/share/zoneinfo/$MYTIMEZONE /etc/localtime
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
-echo "batcave" >> /etc/hostname
-echo "127.0.0.1 localhost batcave" >> /etc/hosts
+echo "$MYHOSTNAME" >> /etc/hostname
+echo "127.0.0.1 localhost $MYHOSTNAME" >> /etc/hosts
 
 clear
 echo "Set Root Password..."
 passwd root
 
-useradd -m -d /home/shreyas shreyas
-echo "Set Password for shreyas..."
+useradd -m -d /home/$MYUSERNAME $MYUSERNAME --badnames
+echo "Set Password for $MYUSERNAME..."
 passwd shreyas
-echo "shreyas ALL=(ALL) ALL" >> /etc/sudoers
+echo "$MYUSERNAME ALL=(ALL) ALL" >> /etc/sudoers
 
 grub-install /dev/sda --efi-directory="/boot"
 grub-mkconfig -o /boot/grub/grub.cfg
