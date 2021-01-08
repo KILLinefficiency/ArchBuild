51f2ba3bd135bca4db671f47e5fcc7294661424d
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-08 12:46:20 +0530

    added some packages

--- ArchBuild_3.sh
+++ ArchBuild_3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S alacritty feh picom xmonad xmonad-contrib xmobar ttf-font-awesome rofi i3lock imagemagick man tree zip unzip wget firefox chromium opera python3 python-pip gcc clang --noconfirm
+sudo pacman -S alacritty feh picom xmonad xmonad-contrib xmobar ttf-hack neofetch htop bat ttf-font-awesome nano vim rofi i3lock imagemagick man tree zip unzip wget firefox chromium opera python3 python-pip gcc clang --noconfirm
