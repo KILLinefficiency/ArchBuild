e222882ad4c498f868c034a1cccccd0e1648235f
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-08 12:39:02 +0530

    deleted few packages

--- ArchBuild_3.sh
+++ ArchBuild_3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S alacritty feh picom xmonad xmonad-contrib xmobar ttf-font-awesome rofi alsa-utils pamixer i3lock imagemagick man tree zip unzip wget firefox chromium opera python3 python-pip gcc clang --noconfirm
+sudo pacman -S alacritty feh picom xmonad xmonad-contrib xmobar ttf-font-awesome rofi i3lock imagemagick man tree zip unzip wget firefox chromium opera python3 python-pip gcc clang --noconfirm
