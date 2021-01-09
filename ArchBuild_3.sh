acd6cc58b8a0315f0a57d8fe3f540756f7ecef2b
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-10 00:30:05 +0530

    changed the structure

--- ArchBuild_3.sh
+++ ArchBuild_3.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-sudo pacman -S alacritty feh picom xmonad xmonad-contrib xmobar ttf-hack neofetch htop bat ttf-font-awesome nano vim rofi i3lock imagemagick man tree zip unzip wget firefox chromium opera python3 python-pip gcc clang --noconfirm
+# Tools for the Window Manager:
+sudo pacman -S alacritty feh picom xmonad xmoand-contrib xmobar ttf-hack ttf-font-awesome rofi i3lock --noconfirm
+
+# Editors:
+sudo pacman -S nano vim --noconfirm
+
+# File Managers:
+sudo pacman -S ranger --noconfirm
+
+# Browsers:
+sudo pacman -S firefox chromium opera --noconfirm
+
+# Developer Tools:
+sudo pacman -S python3 python-pip gcc clang nasm --noconfirm
+
+# Virtualization:
+sudo pacman -S qemu virtualbox virtualbox-host-modules-arch --noconfirm
+
+# Media:
+sudo pacman -S vlc --noconfirm
+
+# Others:
+sudo pacman -S neofetch htop bat imagemagick man tree zip unzip wget --noconfirm
+
+# Drivers:
+sudo modprobe vboxdrv
