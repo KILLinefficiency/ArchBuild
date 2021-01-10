17053a30594dbf68c9094c60108d49ee246aa0a3
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-10 16:56:41 +0530

    made changes

--- ArchBuild_4.sh
+++ ArchBuild_4.sh
@@ -6,7 +6,7 @@ ADDRESS=$(pwd)
 
 asoundconf set-default-card ${MYSOUNDCARD}
 
-mkdir ~/Documents ~/Downloads ~/Music ~/Videos ~/Pictures ~/.aur
+mkdir ~/Documents ~/Downloads ~/Music ~/Videos ~/Pictures ~/Projects ~/Junk ~/.aur ~/.xmonad ~/.config
 cd ~/.aur
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -14,15 +14,16 @@ makepkg -si
 
 cd $ADDRESS
 
-mkdir ~/.xmonad
-mkdir -p ~/.config/rofi
-mkdir -p ~/.config/alacritty
+mkdir ~/.config/rofi
+mkdir ~/.config/zathura
+mkdir ~/.config/alacritty
 
 cp dotfiles/vimrc ~/.vimrc
 cp dotfiles/bashrc ~/.bashrc
 cp dotfiles/xinitrc ~/.xinitrc
+cp dotfiles/zathurarc ~/.config/zathura/zathurarc
 cp dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
-cp dotfiles/alacritty.yml ~/config/alacritty/alacritty.yml
+cp dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 cp -r dotfiles/tools ~/.tools
 
